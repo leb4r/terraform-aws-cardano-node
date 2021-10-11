fd6872b3030cf40dc070e54d0f4ded0d249c01b8
Lucky Baar <git+me@leb4r.io>
2021-10-10 21:26:05 -0500

    iam: add access to cloudwatch log group

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -20,6 +20,11 @@ variable "kms_key_arn" {
   type        = string
 }
 
+variable "log_group_arn" {
+  description = "The ARN of the CloudWatch Log Group"
+  type        = string
+}
+
 variable "ssm_managed" {
   description = "Set to `false` to disable SSM access"
   type        = bool
